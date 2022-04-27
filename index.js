63ccfbba6564fcad05cccb81f6adff3c349a9b6d
Qibin <qbye0405@gmail.com>
2022-04-26 21:01:56 -0400

    commit tree aac556967f4824caec053f96fdfdd88e89886cad

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solutions in this file
+const name = ['Guadalupe', 'Ollie', 'Aki'];
+const event = 'surprise';
+
+function writeCards(name, event) {
+    let message = [];
+    for (let i=0; i < name.length; i++) {
+         message.push (`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    };
+    return message;
+}
+
+console.log(writeCards(name, event));
+
+function countDown(){
+    let i = 10;
+    while (i >= 0) {
+        console.log(i)
+        i--;
+    };
+}
+console.log(countDown());
+
+
+
+
+
+
+
+
+
